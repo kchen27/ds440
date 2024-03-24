3e4d99fbee0f01024920175b4eec41fc6ce815ed
Kyle Chen <kylechen@Kyles-MacBook-Air.local>
2024-03-24 18:27:22 -0400

    model edit

--- linear_dynamic_models.R
+++ linear_dynamic_models.R
@@ -5,11 +5,11 @@ library(tidyverse)
 library(dynlm)
 
 set.seed(123)
-sampled_data <- filtered[sample(nrow(filtered), size = floor(0.01 * nrow(filtered))),]
+sampled_data <- filtered[sample(nrow(filtered), size = floor(0.2 * nrow(filtered))),]
 
 ## NOTES:
 ### No residuals, overfitting the data, aggregate some categories
-linear_model <- lm (Price ~ Name + City + Bedrooms + Bathrooms + Beds + Guests.Included + 
+linear_model <- lm (Price ~ Price + Bedrooms + Bathrooms + Beds + Guests.Included + 
                       Review.Scores.Rating + Review.Scores.Accuracy + Review.Scores.Cleanliness +
                       Review.Scores.Checkin + Review.Scores.Communication + Review.Scores.Location +
                       Review.Scores.Value + Year, data = sampled_data)
@@ -22,9 +22,12 @@ summary(linear_model)
 # Feeding the model inflation index
 # Cross sectional model/ Inflation
 
+# Initial process start off with just prediction based on the factors and include inflations later on
+# 
 
 
-dynamic_model <- dynlm( Price ~ L(Price, 1)  + Name + City + Bedrooms + Bathrooms + Beds + Guests.Included + 
+
+dynamic_model <- dynlm( Price ~ L(Price, 1) City + Bedrooms + Bathrooms + Beds + Guests.Included + 
                           Review.Scores.Rating + Review.Scores.Accuracy + Review.Scores.Cleanliness +
                           Review.Scores.Checkin + Review.Scores.Communication + Review.Scores.Location +
                           Review.Scores.Value + Year, data = sampled_data)
